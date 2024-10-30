eb0039773caa3ba86ea6e6d924c8a96c5bc7396e
wlals <jiminelp@gmail.com>
2024-10-31 00:55:35 +0900

    setting :: axios 세팅

---
+++ hackathon6/src/apis/axios.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const instance = axios.create({
+  baseURL:
+    "https://port-0-swing-call-backend-754g42aluv3zyvy.sel5.cloudtype.app/",
+  timeout: 10000,
+});
