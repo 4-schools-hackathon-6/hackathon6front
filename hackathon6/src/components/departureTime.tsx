b626eb5f962b9bfa788bff96d2c465f8f5eb28a9
wlals <jiminelp@gmail.com>
2024-10-30 19:38:42 +0900

    feat :: 남은 시간 컴포넌트 퍼블리싱

---
+++ hackathon6/src/components/departureTime.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { Bicycle2, Kickboard, Scooter } from "@/app/assets";
+import { useEffect, useState } from "react";
+
+interface PropsType {
+  hour: number;
+  min: number;
+  mobility: "kickboard" | "bicycle" | "scooter";
+  className?: string;
+  mode: "call" | "reservation";
+}
+
+export const DepartureTime = ({
+  hour,
+  min,
+  mobility,
+  className,
+  mode,
+}: PropsType) => {
+  const [timeRemaining, setTimeRemaining] = useState("");
+
+  useEffect(() => {
+    setTimeRemaining(calculateTimeRemaining());
+    const interval = setInterval(() => {
+      setTimeRemaining(calculateTimeRemaining());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [hour, min]);
+
+  const calculateTimeRemaining = () => {
+    const now = new Date();
+    let target = new Date();
+
+    const daysToAdd = Math.floor(hour / 24);
+    const normalizedHour = hour % 24;
+
+    target.setHours(normalizedHour, min, 0);
+
+    if (daysToAdd > 0) {
+      target.setDate(target.getDate() + daysToAdd);
+    }
+
+    if (target < now) {
+      target.setDate(target.getDate() + 1);
+    }
+
+    const diffMs = target - now;
+    const diffHours = Math.floor(diffMs / (1000 * 60 * 60));
+    const diffMinutes = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
+    const diffSec = Math.floor((diffMs % (1000 * 60)) / 1000);
+
+    return `약 ${
+      diffHours > 0 ? `${diffHours.toString().padStart(2, "0")}시간` : ""
+    } ${diffMinutes.toString().padStart(2, "0")}:${diffSec
+      .toString()
+      .padStart(2, "0")}분 후`;
+  };
+  return (
+    <div className={`${className} bg-white w-full px-7 py-3 rounded-t-2xl`}>
+      <div className="flex gap-2 items-end">
+        {mobility === "bicycle" ? (
+          <Bicycle2 />
+        ) : mobility === "kickboard" ? (
+          <Kickboard />
+        ) : (
+          <Scooter />
+        )}
+        <span className="font-semibold text-[20px] leading-6">
+          최미래님
+          <br />
+          출발 예정 시간
+        </span>
+      </div>
+      <span className="text-[10px] text-orange-500">
+        {mode === "call"
+          ? "콜 서비스는 10 ~ 30분 이내로만 설정 할 수 있어요"
+          : "출발 시각 30분 전 자동으로 콜서비스가 호출됩니다."}
+      </span>
+      <div className="flex justify-end font-bold text-[22px]">
+        {timeRemaining}
+      </div>
+      <button className="w-full bg-black text-white py-4 rounded-lg font-semibold text-lg">
+        완료
+      </button>
+    </div>
+  );
+};
